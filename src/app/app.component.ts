b838f4a8a305cf106d8297746ee891824cad5144
Daniel Hernandez <daniel0808soares@gmail.com>
2023-10-10 22:31:30 -0300

    4 - Ciclo ngOnDestroy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,4 +25,13 @@ export class AppComponent implements OnInit, DoCheck{
     this.itemParaSerEditado = item;
   }
 
+  deletarItem(id:number){
+    const index = this.listaDeCompra.findIndex((item)=> item.id === id);
+    this.listaDeCompra.splice(index, 1);
+  }
+
+  limparLista(){
+    this.listaDeCompra = [];
+  }
+
 }
